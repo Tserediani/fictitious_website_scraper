821cda61111b9e508476b93cd7ed481b52e4c0f3
Giorgi Tserediani <89919081+Tserediani@users.noreply.github.com>
2023-05-25 15:37:26 +0400

    constants.py

--- common/constants.py
+++ common/constants.py
@@ -3,5 +3,5 @@ import os
 
 WORKING_DIR = os.path.abspath(os.path.dirname(__name__))  ##Current Directory
 FILENAME = "output_file"
-BASE_DOMAIN = ""
-BASE_URL = ""
+BASE_DOMAIN = ""##Fictitious base domain
+BASE_URL = ""##Fictitious base url
